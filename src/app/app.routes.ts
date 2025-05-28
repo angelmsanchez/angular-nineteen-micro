16c13e066a086fd3f032e525e5ace77c02ec3a1d
Angel Muñoz <tok.amunoz@iberia.es>
2025-05-28 17:34:06 +0200

    add routing microfrontend

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    // loadChildren: () =>
+    //   import('./home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+    // loadChildren: () =>
+    //   import('./about/about.component').then((m) => m.AboutComponent),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
